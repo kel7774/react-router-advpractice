13655b301183b67d6f687a9d1d3b9d0f1aa38df2
Kelli Landry <landry.kel@gmail.com>
2021-06-12 12:32:46 -0500

    Use query parameters for sorting & showing in URL

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,12 +1,33 @@
 
 import QuoteItem from './QuoteItem'
 import classes from './QuoteList.module.css'
-
+import { useHistory, useLocation } from 'react-router-dom'
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1
+    }
+  })
+}
 const QuoteList = (props) => {
+  const history = useHistory()
+  const location = useLocation()
+  const queryParams = new URLSearchParams(location.search)
+  const isSortingAsc = queryParams.get('sort') === 'asc'
+  console.log(location)
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAsc)
+  const changeSortingHandler = () => {
+    history.push('/quotes?sort=' + (isSortingAsc ? 'desc' : 'asc'))
+  }
   return (
     <>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>Sort {isSortingAsc ? 'Descending' : 'Ascending'}</button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
